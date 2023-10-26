0916e129ac99222ac8af42db2e4e807621f4260e
A-fethi <fethiabderrahmane1@gmail.com>
2023-10-26 12:48:16 -0700

    tasks 0-2-3-4-5

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Principale applications module"""
+from models import storage
+from flask import Flask, jsonify
+from os import getenv
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Removes the current SQLALchemy Session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(exception):
+    data = {"error": "Not found"}
+    return jsonify(data)
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", '0.0.0.0')
+    port = int(getenv("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port, threaded=True)
