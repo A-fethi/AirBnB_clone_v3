3cd80c24d42e209f5791e4dcdd8eb18bd50d1be6
A-fethi <fethiabderrahmane1@gmail.com>
2023-10-30 04:05:06 -0700

    Enhacement

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -20,50 +20,49 @@ def get_all_users():
 def get_user_by_id(user_id):
     """Retrieves a User object"""
     user = storage.get(User, user_id)
-    if user:
-        return jsonify(user.to_dict())
-    abort(404)
+    if not user:
+        abort(404)
+    return jsonify(user.to_dict())
 
 
 @app_views.route('/users/<user_id>', methods=["DELETE"])
 def delete_user_by_id(user_id):
     """Deletes a User object"""
     user = storage.get(User, user_id)
-    if user:
-        storage.delete(user)
-        storage.save()
-        return jsonify({}), 200
-    abort(404)
+    if not user:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return jsonify({}), 200
 
 
 @app_views.route('/users/', methods=["POST"])
 def post_user():
     """Creates a User"""
-    json_data = request.get_json()
-    if not json_data:
+    if not request.get_json():
         abort(400, description="Not a JSON")
-    if 'email' not in json_data:
+    if 'email' not in request.get_json():
         abort(400, description="Missing email")
-    if 'password' not in json_data:
+    if 'password' not in request.get_json():
         abort(400, description="Missing password")
+    json_data = request.get_json()
     new_user = User(**json_data)
-    storage.new(new_user)
-    storage.save()
+    new_user.save()
     return jsonify(new_user.to_dict()), 201
 
 
 @app_views.route('/users/<user_id>', methods=["PUT"])
 def put_user(user_id):
     """Updates a User object"""
-    json_data = request.get_json()
     user = storage.get(User, user_id)
-    if not json_data:
+    if not user:
+        abort(404)
+    if not request.get_json():
         abort(400, description="Not a JSON")
-    if user:
-        for k, v in json_data.items():
-            ignored_keys = ["id", "email", "created_at", "updated_at"]
-            if k not in ignored_keys:
-                setattr(user, k, v)
-        storage.save()
-        return jsonify(user.to_dict()), 200
-    abort(404)
+    ignored_keys = ["id", "email", "created_at", "updated_at"]
+    json_data = request.get_json()
+    for k, v in json_data.items():
+        if k not in ignored_keys:
+            setattr(user, k, v)
+    storage.save()
+    return jsonify(user.to_dict()), 200
