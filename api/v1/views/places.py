b55dcb3ab630ffbd3a7c6874da863b9582fc0dd4
A-fethi <fethiabderrahmane1@gmail.com>
2023-10-30 04:59:56 -0700

    Enhacement

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,7 +8,8 @@ from models.user import User
 from models import storage
 
 
-@app_views.route("/cities/<city_id>/places/", methods=["GET"])
+@app_views.route("/cities/<city_id>/places", methods=["GET"],
+                 strict_slashes=False)
 def get_places_by_city(city_id):
     """Retrieves the list of all Place objects of a City"""
     city = storage.get(City, city_id)
@@ -21,7 +22,8 @@ def get_places_by_city(city_id):
     return jsonify(list_places)
 
 
-@app_views.route('/places/<place_id>', methods=["GET"])
+@app_views.route('/places/<place_id>', methods=["GET"],
+                 strict_slashes=False)
 def get_place_by_id(place_id):
     """Retrieves a Place object"""
     place = storage.get(Place, place_id)
@@ -30,7 +32,8 @@ def get_place_by_id(place_id):
     abort(404)
 
 
-@app_views.route('/places/<place_id>', methods=["DELETE"])
+@app_views.route('/places/<place_id>', methods=["DELETE"],
+                 strict_slashes=False)
 def delete_place_by_id(place_id):
     """Deletes a Place object"""
     place = storage.get(Place, place_id)
@@ -41,7 +44,8 @@ def delete_place_by_id(place_id):
     abort(404)
 
 
-@app_views.route('/cities/<city_id>/places/', methods=["POST"])
+@app_views.route('/cities/<city_id>/places', methods=["POST"],
+                 strict_slashes=False)
 def post_place(city_id):
     """Creates a Place"""
     cit = storage.get(City, city_id)
@@ -64,7 +68,8 @@ def post_place(city_id):
     return jsonify(new_place.to_dict()), 201
 
 
-@app_views.route('/places/<place_id>', methods=["PUT"])
+@app_views.route('/places/<place_id>', methods=["PUT"],
+                 strict_slashes=False)
 def put_place(place_id):
     """Updates a Place object"""
     json_data = request.get_json()
